afaae7c8c363dfd4a304f9ec732d568e9a60c048
Matthias Petursson <matthias01123@gmail.com>
2019-04-10 16:53:16 +0000

    Change background collision arrays to arrays of strings instead of numbers for ease of manipulating and creating the collision strings.

--- js/tools/consts.js
+++ js/tools/consts.js
@@ -30,13 +30,13 @@ consts['KEY_B'] = 'B'.charCodeAt(0);
 consts['KEY_C'] = 'C'.charCodeAt(0);
 
 // background consts
-consts['NOBLOCK'] = 0;
-consts['REGBLOCK'] = 1; // regular block
-consts['PLATFORMBLOCK'] = 2; // block you can jump through but land on
-consts['STAIRBLOCK'] = 3;
-consts['TELEBLOCK'] = 4; // teleport block, special within each scene to go to another scene
-consts['STAIRTOPBLOCK'] = 5; // the top of all stairs (which you can stand on, as opposed to regular stair blocks)
-consts['SECONDARYTELEBLOCK'] = 6; // if there are two teleports on one scene, this behaves exactly as teleblock otherwise
+consts['NOBLOCK'] = '0';
+consts['REGBLOCK'] = '1'; // regular block
+consts['PLATFORMBLOCK'] = '2'; // block you can jump through but land on
+consts['STAIRBLOCK'] = '3';
+consts['TELEBLOCK'] = '4'; // teleport block, special within each scene to go to another scene
+consts['STAIRTOPBLOCK'] = '5'; // the top of all stairs (which you can stand on, as opposed to regular stair blocks)
+consts['SECONDARYTELEBLOCK'] = '6'; // if there are two teleports on one scene, this behaves exactly as teleblock otherwise
 								  // requests the 'secondary-special' from background instead of the normal special	
 
 global.set('consts', consts);
