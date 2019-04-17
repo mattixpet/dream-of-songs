371a28473e6ba32ce5371366a670722679d24afe
Matthias Petursson <matthias01123@gmail.com>
2019-04-17 16:05:15 +0000

    Finish about menu, add splitting text for scrolling.

--- js/menus/AboutMenu.js
+++ js/menus/AboutMenu.js
@@ -17,20 +17,30 @@ function AboutMenu () {
 	ScrollableMenu.call(this, data.upArrowPos, data.downArrowPos);
 
 	this.buttonActions['back'] = this._handleBack;
+
+	// populated in this._splitText()
+	// depending on how much we can fit on one page (and then the scroll goes between pages here)
+	// this.text.length num pages
+	this.text = [];
+	this.textIndex = 0;
+	this._splitText();
 }
 
 AboutMenu.prototype = Object.create(ScrollableMenu.prototype);
 
 AboutMenu.prototype._handleBack = function () {
 	global.get(this.previousMenu).display();
+	this.textIndex = 0;
 };
 
 AboutMenu.prototype._handleUp = function () {
-	console.log('up');
+	this.textIndex = this.textIndex - 1 === -1 ? this.text.length - 1 : this.textIndex - 1;
+	this.draw();
 };
 
 AboutMenu.prototype._handleDown = function () {
-	console.log('down');
+	this.textIndex = (this.textIndex + 1) % this.text.length;
+	this.draw();
 };
 
 AboutMenu.prototype.onEnter = function () {
@@ -51,7 +61,7 @@ AboutMenu.prototype._drawText = function () {
 
 	draw.writeText(
 		global.get('ctx'),
-		data.text,
+		this.text[this.textIndex],
 		data.textPos.x,
 		data.textPos.y,
 		common.font,
@@ -85,6 +95,23 @@ AboutMenu.prototype._drawBackButton = function () {
 		common.fontSize,
 		common.fontColor
 	);
+};
+
+
+// Take the entire text in menu-text-data.aboutMenu.text and split
+// it into pages that can fit for us (then scroll between)
+// Populates this.text array
+AboutMenu.prototype._splitText = function () {
+	// we can fit roughly numWordsPerPage words on each page
+	var numWordsPerPage = 110;
+	// menu-text-data.aboutMenu.text should be split on newlines which can fit each of our page
+	var words = global.get('menu-text-data')[this.name].text.split(' ');
+	var wordsDone = 0;
+	while (wordsDone + numWordsPerPage < words.length) {
+		this.text.push(words.slice(wordsDone, wordsDone+numWordsPerPage).join(' '));
+		wordsDone += numWordsPerPage;
+	}
+	this.text.push(words.slice(wordsDone, words.length).join(' '));
 };
 
 global.set('class/AboutMenu', AboutMenu);
